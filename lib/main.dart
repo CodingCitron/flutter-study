75fa5a6daa75b7ece5daf130603914c6128fb694
sangmin <xectler@gmail.com>
2023-11-08 02:49:54 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,13 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818), /* ARGB */
+        backgroundColor: const Color(0xFF181818), /* ARGB */
         body: Padding(
           padding: EdgeInsets.symmetric(
             horizontal: 40
           ),
           child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 80,
@@ -41,6 +42,45 @@ class App extends StatelessWidget {
                           ),
                         ),
                       ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white.withOpacity(0.8)
+                  ),
+                ),
+                SizedBox(height: 5,),
+                Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                      fontSize: 42,
+                      color: Colors.white.withOpacity(0.8),
+                      fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50,),
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(
+                            fontSize: 22,
+                          ),
+                        ),
+                      ),
                     )
                   ],
                 )
